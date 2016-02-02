519641a6a4344b74dc192a7432a18b26ce1a8523
Rolando Cosme <rolandocosme@nbp-49-232.nbp.ruw.rutgers.edu>
2016-02-01 19:28:15 -0500

    created 2 servers on server.js with random display strings

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
 var http = require('http');
 
-var port = 8080;
+var PORT1 = 7000;
+var PORT2 = 7500;
+var good = ["awesome","cool", "bro"];
+var bad = ["not awesome","not cool", "not bro"];
+var randGood = good[Math.floor(Math.random() * good.length)];
+var randBad = bad[Math.floor(Math.random() * bad.length)];
 
-
-var server = http.createServer( function (req, res) {
-  res.end ("it worked! you hit:" + req.url);
+var server1 = http.createServer( function (req, res) {
+   res.end (randGood);
 });
 
-
-
-server.listen (PORT, function () {
-  console.log( "server is listening at http://lolcalhost:%s", PORT);
+server1.listen ( PORT1, function () {
+  console.log( "server is listening at http://lolcalhost:%s", PORT1);
 });
 
+var server2 = http.createServer( function (req, res) {
+  res.end (randBad);
+});
 
-
+server2.listen ( PORT2, function () {
+  console.log( "server is listening at http://lolcalhost:%s", PORT2);
+});
 
 //localhost:8080
+
+// Create an app that has two web servers.
+// One that listens on port 7000 and one that listens on port 7500.
+// The one listening on port 7000 will always tell the user something good about themselves.
+// The one listening on 7500 will always tell the user something bad about themselves.
+// Make sure you create a Github repo and commit this code!
+// ** Bonus **
+
+// Generate the good / bad phrase randomly from a list of predefined phrases
+// Use the twitter package inside the response to also return a random tweet!
